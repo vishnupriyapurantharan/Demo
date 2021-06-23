ecce13fdd436086fc1373b5df8e6d094dcc0fe96
40010753 <bharath.g@ltts.com>
2021-06-23 16:18:14 +0530

    First Commit

---
+++ Hello.c
@@ -0,0 +1,21 @@
+/**
+ * @file Hello.c
+ * @author Bharath G (Bharath.g@ltts.com)
+ * @brief First C Program file
+ * @version 0.1
+ * @date 2021-06-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "stdio.h"
+int Var = 100;
+int main()
+{
+    for(int index = 0; index< 10; index++)
+        printf("Welcome to STEPin Program, Var = %d", Var+index);
+    
+    return 0;
+}
+
+/* gcc filename.c -o hello.exe */
